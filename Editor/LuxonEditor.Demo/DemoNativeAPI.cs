7d397112518942a35e9a9fa7f108d59a161717df
Sina Rahmani <rahmani.sina1992@gmail.com>
2026-04-15 17:05:33 +0100

    Renamed the demo API project and resolved the dependencies.

--- Editor/LuxonEditor.Demo/DemoNativeAPI.cs
+++ Editor/LuxonEditor.Demo/DemoNativeAPI.cs
@@ -16,23 +16,23 @@ namespace LuxonEditor.Demo
 
     internal static partial class DemoNativeAPI
     {
-        [DllImport("QuantumEngine.DemoAPI.dll", EntryPoint = "Run_Simple_Scene")]
+        [DllImport("LuxonEngine.DemoAPI.dll", EntryPoint = "Run_Simple_Scene")]
         public static extern bool RunSimpleScene(IntPtr handle, GraphicAPI graphicAPI, RenderMode renderMode);
 
 
-        [DllImport("QuantumEngine.DemoAPI.dll", EntryPoint = "Run_Reflection_Scene")]
+        [DllImport("LuxonEngine.DemoAPI.dll", EntryPoint = "Run_Reflection_Scene")]
         public static extern bool RunReflectionScene(IntPtr handle, GraphicAPI graphicAPI, RenderMode renderMode);
 
        
-        [DllImport("QuantumEngine.DemoAPI.dll", EntryPoint = "Run_Shadow_Scene")]
+        [DllImport("LuxonEngine.DemoAPI.dll", EntryPoint = "Run_Shadow_Scene")]
         public static extern bool RunShadowScene(IntPtr handle, GraphicAPI graphicAPI);
 
 
-        [DllImport("QuantumEngine.DemoAPI.dll", EntryPoint = "Run_Refraction_Scene")]
+        [DllImport("LuxonEngine.DemoAPI.dll", EntryPoint = "Run_Refraction_Scene")]
         public static extern bool RunRefractionScene(IntPtr handle, GraphicAPI graphicAPI);
 
 
-        [DllImport("QuantumEngine.DemoAPI.dll", EntryPoint = "Run_Complete_Scene")]
+        [DllImport("LuxonEngine.DemoAPI.dll", EntryPoint = "Run_Complete_Scene")]
         public static extern bool RunCompleteScene(IntPtr handle, GraphicAPI graphicAPI);
     }
 }
