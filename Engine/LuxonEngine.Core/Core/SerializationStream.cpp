f7a405f4f0ddfafaa2ef3d7239b6a464ba5821e4
Sina Rahmani <rahmani.sina1992@gmail.com>
2026-06-15 21:35:24 +0100

    Added a stream class for serialization/deserialization interface.

---
+++ Engine/LuxonEngine.Core/Core/SerializationStream.cpp
@@ -0,0 +1,276 @@
+#include "SerializationStream.h"
+
+#include "../Core/Color.h"
+#include "../Core/Vector2.h"
+#include "../Core/Vector3.h"
+
+#include <boost/uuid/string_generator.hpp>
+#include <boost/json.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+#include <fstream>
+
+#ifdef _DEBUG
+#pragma comment(lib, "libboost_json-vc143-mt-gd-x64-1_90.lib")
+#pragma comment(lib, "libboost_container-vc143-mt-gd-x64-1_90.lib")
+#else
+#pragma comment(lib, "libboost_json-vc143-mt-x64-1_90.lib")
+#pragma comment(lib, "libboost_container-vc143-mt-x64-1_90.lib")
+#endif
+
+namespace LuxonEngine {
+
+    SerializationStream::SerializationStream()
+        : m_jsonData(boost::json::object())
+    {
+    }
+
+    SerializationStream::SerializationStream(boost::json::value& data)
+        : m_jsonData(data)
+    {
+    }
+
+    bool SerializationStream::LoadFromFile(const std::string& filePath)
+    {
+        std::ifstream file(filePath, std::ios::in | std::ios::binary);
+        if (!file.is_open()) {
+            m_lastError = "Failed to open file: " + filePath;
+            return false;
+        }
+
+        /*std::string jsonContent(
+            (std::istreambuf_iterator<char>(file)),
+            std::istreambuf_iterator<char>()
+        );*/
+        
+
+        boost::system::error_code ec;
+        m_jsonData = boost::json::parse(file, ec);
+        file.close();
+        if (ec) {
+            m_lastError = "Failed to parse JSON: " + ec.message();
+            return false;
+        }
+
+        return true;
+    }
+
+    bool SerializationStream::SaveToFile(const std::string& filePath)
+    {
+        std::ofstream file(filePath, std::ios::out | std::ios::binary);
+        if (!file.is_open()) {
+            m_lastError = "Failed to open file for writing: " + filePath;
+            return false;
+        }
+
+        std::string jsonString = boost::json::serialize(m_jsonData);
+        file.write(jsonString.c_str(), jsonString.length());
+        file.close();
+
+        return true;
+    }
+
+    int SerializationStream::GetInt(const std::string& fieldName, int defaultValue)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                return static_cast<int>(obj[fieldName].as_int64());
+            }
+        }
+        catch (const std::exception&) {
+            // Return default on error
+        }
+        return defaultValue;
+    }
+
+    Float SerializationStream::GetFloat(const std::string& fieldName, Float defaultValue)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                return static_cast<Float>(obj[fieldName].as_double());
+            }
+        }
+        catch (const std::exception&) {
+            // Return default on error
+        }
+        return defaultValue;
+    }
+
+    bool SerializationStream::GetVector2(const std::string& fieldName, Vector2& vec)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                auto& vecObj = obj[fieldName].as_object();
+                Float x = static_cast<Float>(vecObj["x"].as_double());
+                Float y = static_cast<Float>(vecObj["y"].as_double());
+                vec = Vector2(x, y);
+                return true;
+            }
+        }
+        catch (const std::exception&) {
+            // Return default on error
+        }
+        vec = Vector2();
+        return false;
+    }
+
+    bool SerializationStream::GetVector3(const std::string& fieldName, Vector3& vec)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                auto& vecObj = obj[fieldName].as_object();
+                Float x = static_cast<Float>(vecObj["x"].as_double());
+                Float y = static_cast<Float>(vecObj["y"].as_double());
+                Float z = static_cast<Float>(vecObj["z"].as_double());
+                vec = Vector3(x, y, z);
+                return true;
+            }
+        }
+        catch (const std::exception&) {
+            // Return default on error
+        }
+        vec = Vector3();
+        return false;
+    }
+
+    bool SerializationStream::GetColor(const std::string& fieldName, Color& color)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                auto& colorObj = obj[fieldName].as_object();
+                Float r = static_cast<Float>(colorObj["r"].as_double());
+                Float g = static_cast<Float>(colorObj["g"].as_double());
+                Float b = static_cast<Float>(colorObj["b"].as_double());
+                Float a = static_cast<Float>(colorObj["a"].as_double());
+                color = Color(r, g, b, a);
+                return true;
+            }
+        }
+        catch (const std::exception&) {
+            // Return default on error
+        }
+        color = Color();
+    }
+
+    bool SerializationStream::GetString(const std::string& fieldName, std::string& str)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                str = obj[fieldName].as_string();
+                return true;
+            }
+        }
+        catch (const std::exception&) {
+            // Return default on error
+        }
+        return false;
+    }
+
+    GUID SerializationStream::GetGuid(const std::string& fieldName)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                std::string guidStr = std::string(obj[fieldName].as_string());
+                boost::uuids::string_generator gen;
+                return gen(guidStr);
+            }
+        }
+        catch (const std::exception&) {
+            // Return nil UUID on error
+        }
+        return boost::uuids::uuid();
+    }
+
+    SerializationStream SerializationStream::Object(const std::string& fieldName)
+    {
+        try {
+            auto& obj = m_jsonData.as_object();
+            if (obj.contains(fieldName)) {
+                return SerializationStream(obj[fieldName]);
+            }
+        }
+        catch (const std::exception&) {
+            // Return nil UUID on error
+        }
+        return SerializationStream();
+    }
+
+    void SerializationStream::SetInt(const std::string& fieldName, int value)
+    {
+        EnsureFieldExists(fieldName);
+        m_jsonData.as_object()[fieldName] = value;
+    }
+
+    void SerializationStream::SetFloat(const std::string& fieldName, Float value)
+    {
+        EnsureFieldExists(fieldName);
+        m_jsonData.as_object()[fieldName] = static_cast<double>(value);
+    }
+
+    void SerializationStream::SetVector2(const std::string& fieldName, const Vector2& vec)
+    {
+        EnsureFieldExists(fieldName);
+        auto& obj = m_jsonData.as_object();
+        boost::json::object vecObj;
+        vecObj["x"] = static_cast<double>(vec.x);
+        vecObj["y"] = static_cast<double>(vec.y);
+        obj[fieldName] = vecObj;
+    }
+
+    void SerializationStream::SetVector3(const std::string& fieldName, const Vector3& vec)
+    {
+        EnsureFieldExists(fieldName);
+        auto& obj = m_jsonData.as_object();
+        boost::json::object vecObj;
+        vecObj["x"] = static_cast<double>(vec.x);
+        vecObj["y"] = static_cast<double>(vec.y);
+        vecObj["z"] = static_cast<double>(vec.z);
+        obj[fieldName] = vecObj;
+    }
+
+    void SerializationStream::SetColor(const std::string& fieldName, const Color& color)
+    {
+        EnsureFieldExists(fieldName);
+        auto& obj = m_jsonData.as_object();
+        boost::json::object colorObj;
+        Float* colorArray = const_cast<Color&>(color).GetColorArray();
+        colorObj["r"] = static_cast<double>(colorArray[0]);
+        colorObj["g"] = static_cast<double>(colorArray[1]);
+        colorObj["b"] = static_cast<double>(colorArray[2]);
+        colorObj["a"] = static_cast<double>(colorArray[3]);
+        obj[fieldName] = colorObj;
+    }
+
+    void SerializationStream::SetString(const std::string& fieldName, const std::string& str)
+    {
+        EnsureFieldExists(fieldName);
+        m_jsonData.as_object()[fieldName] = str;
+    }
+
+    void SerializationStream::SetGuid(const std::string& fieldName, const GUID& guid)
+    {
+        EnsureFieldExists(fieldName);
+        m_jsonData.as_object()[fieldName] = boost::uuids::to_string(guid);
+    }
+
+    void SerializationStream::SetObject(const std::string& fieldName, const SerializationStream& object)
+    {
+        EnsureFieldExists(fieldName);
+        m_jsonData.as_object()[fieldName] = object.m_jsonData;
+    }
+
+    void SerializationStream::EnsureFieldExists(const std::string& fieldName)
+    {
+        auto& obj = m_jsonData.as_object();
+        if (!obj.contains(fieldName)) {
+            obj[fieldName] = boost::json::value();
+        }
+    }
+}
---
+++ Engine/LuxonEngine.Core/Core/SerializationStream.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "../BasicTypes.h"
+#include "../export.h"
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/json.hpp>
+
+#include <string>
+#include <fstream>
+
+namespace LuxonEngine {
+    using GUID = boost::uuids::uuid;
+    // Forward declarations
+    class Color;
+    class Vector2;
+    class Vector3;
+
+    class LUXON_CORE_API SerializationStream {
+    public:
+        // Constructor - initializes empty JSON
+        SerializationStream();
+
+        // Load JSON from file
+        bool LoadFromFile(const std::string& filePath);
+
+        // Save JSON to file
+        bool SaveToFile(const std::string& filePath);
+
+        // Get methods
+        int GetInt(const std::string& fieldName, int defaultValue = 0);
+        Float GetFloat(const std::string& fieldName, Float defaultValue = 0.0f);
+        bool GetVector2(const std::string& fieldName, Vector2& vec);
+        bool GetVector3(const std::string& fieldName, Vector3& vec);
+        bool GetColor(const std::string& fieldName, Color& color);
+        bool GetString(const std::string& fieldName, std::string& str);
+        GUID GetGuid(const std::string& fieldName);
+        SerializationStream Object(const std::string& fieldName);
+
+        // Set methods
+        void SetInt(const std::string& fieldName, int value);
+        void SetFloat(const std::string& fieldName, Float value);
+        void SetVector2(const std::string& fieldName, const Vector2& vec);
+        void SetVector3(const std::string& fieldName, const Vector3& vec);
+        void SetColor(const std::string& fieldName, const Color& color);
+        void SetString(const std::string& fieldName, const std::string& str);
+        void SetGuid(const std::string& fieldName, const GUID& guid);
+        void SetObject(const std::string& fieldName, const SerializationStream& object);
+    private:
+        SerializationStream(boost::json::value& data);
+        boost::json::value m_jsonData;
+        std::string m_lastError;
+
+        // Helper method to ensure field exists as object
+        void EnsureFieldExists(const std::string& fieldName);
+    };
+}
